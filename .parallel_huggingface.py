b0a30e0eb8ab40fa172058d21c68e6420209ebcb
bcritt1 <bcritt@stanford.edu>
2023-11-09 14:51:50 -0800

    add hidden files

---
+++ .parallel_huggingface.py
@@ -0,0 +1,29 @@
+# Import packages
+import os
+import json
+import sys
+
+# Read in corpus
+user = os.getenv('USER')
+corpusdir = '/farmshare/learning/data/emerson/'.format(user)
+corpus = []
+x = os.listdir(corpusdir)
+infile = x[int(sys.argv[1])]
+#for infile in os.listdir(corpusdir):
+with open(corpusdir+infile, errors='ignore') as fin:
+    corpus.append(fin.read())
+
+# Import language models and pipeline elements
+from transformers import AutoTokenizer, AutoModelForTokenClassification
+tokenizer = AutoTokenizer.from_pretrained("Jean-Baptiste/roberta-large-ner-english")
+model = AutoModelForTokenClassification.from_pretrained("Jean-Baptiste/roberta-large-ner-english")
+
+
+# Process text sample (from wikipedia)
+from transformers import pipeline
+nlp = pipeline('ner', model=model, tokenizer=tokenizer, aggregation_strategy="simple")
+entities = nlp(corpus)
+
+# Export data to json
+with open('/scratch/users/{}/outputs/data.json'.format(user), 'w', encoding='utf-8') as f:
+    json.dump(str(entities), f, ensure_ascii=False, indent=4)
